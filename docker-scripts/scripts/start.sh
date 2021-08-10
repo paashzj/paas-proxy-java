7dc2517b78278e4d67b00898d9b17e5a1c0f5dbf
shoothzj <shoothzj@gmail.com>
2021-08-10 20:19:50 +0800

    Add console log and error log

--- docker-scripts/scripts/start.sh
+++ docker-scripts/scripts/start.sh
@@ -4,4 +4,6 @@ cd ..
 
 echo `pwd`
 
-java -Xmx1G -Xms1G -XX:MaxDirectMemorySize=2G -Dlog4j.configurationFile=/opt/sh/conf/log4j2.xml -jar /opt/sh/paas-proxy.jar
+mkdir /opt/sh/logs
+
+java -Xmx1G -Xms1G -XX:MaxDirectMemorySize=2G -Dlog4j.configurationFile=/opt/sh/conf/log4j2.xml -jar /opt/sh/paas-proxy.jar >/opt/sh/logs/console.log 2>/opt/sh/logs/error.log
