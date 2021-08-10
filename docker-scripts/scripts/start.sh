6df2759a641339504ffacd9c603945af1b5fad8a
shoothzj <shoothzj@gmail.com>
2021-08-10 18:34:57 +0800

    Add spring boot actuator

--- docker-scripts/scripts/start.sh
+++ docker-scripts/scripts/start.sh
@@ -4,4 +4,4 @@ cd ..
 
 echo `pwd`
 
-java -Xmx1G -Xms1G -XX:MaxDirectMemorySize=2G -Dlog4j.configurationFile=/opt/sh/conf/log4j2.xml -jar /opt/sh/ttbb.jar
+java -Xmx1G -Xms1G -XX:MaxDirectMemorySize=2G -Dlog4j.configurationFile=/opt/sh/conf/log4j2.xml -jar /opt/sh/paas-proxy.jar
