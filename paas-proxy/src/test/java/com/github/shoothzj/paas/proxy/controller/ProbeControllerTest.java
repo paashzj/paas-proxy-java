cfc5f54b9cb1fd5e130b9633a6c7d25ad5caaecf
LeKe <95280282+goflutterjava@users.noreply.github.com>
2022-07-12 15:11:49 +0800

    add readiness controller (#47)

--- paas-proxy/src/test/java/com/github/shoothzj/paas/proxy/controller/ProbeControllerTest.java
+++ paas-proxy/src/test/java/com/github/shoothzj/paas/proxy/controller/ProbeControllerTest.java
@@ -17,11 +17,9 @@
  * under the License.
  */
 
-package com.github.shoothzj.paas.proxy.pulsar.http.controller;
+package com.github.shoothzj.paas.proxy.controller;
 
-import com.github.shoothzj.paas.common.proxy.http.module.ProduceMsgReq;
 import com.github.shoothzj.paas.proxy.test.config.TestConfig;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -31,21 +29,17 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@Slf4j
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = TestConfig.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class ProducerControllerTest {
+class ProbeControllerTest {
 
     @Autowired
     private TestRestTemplate template;
 
     @Test
     void testProduceMsgNull() throws Exception {
-        ProduceMsgReq produceMsgReq = new ProduceMsgReq();
-        ResponseEntity<String> entity = template.postForEntity("/v1/pulsar/tenants/public/namespaces/default/topics/topic/produce",
-                produceMsgReq, String.class);
-        log.info("entity is {}", entity);
-        Assertions.assertEquals(500, entity.getStatusCodeValue());
+        ResponseEntity<String> entity = template.getForEntity("/v1/readiness", String.class);
+        Assertions.assertEquals(200, entity.getStatusCodeValue());
     }
 
 }
